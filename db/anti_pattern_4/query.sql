c5730bd8bee9e814ee355b3a4c53db6ed5794192
Naoki Kaneko <lpeopleocomevtogethere@gmail.com>
2022-10-15 22:41:19 +0900

    全体的に修正

--- db/anti_pattern_4/query.sql
+++ db/anti_pattern_4/query.sql
@@ -13,40 +13,40 @@ create table if not exists message(
 create table if not exists closure(
     parent_id int,
     child_id int,
-    depth int,
+    offset int,
     PRIMARY KEY (parent_id, child_id)
 );
 
 insert into message(id, text, created_at) values (1, "1: 昨日何時に寝た？", now());
 insert into message(id, text, created_at) values (2, "2: >>1 ８時", now());
 insert into message(id, text, created_at) values (3, "3: >>1 １１時", now());
-
 insert into message(id, text, created_at) values (4, "4: >>2 早い！", now());
 insert into message(id, text, created_at) values (5, "5: >>2 なんでそんな早い？", now());
 
--- B（第一階層）
-insert into closure(parent_id, child_id) values (1, 1);
-insert into closure(parent_id, child_id) values (2, 2);
-insert into closure(parent_id, child_id) values (3, 3);
-insert into closure(parent_id, child_id) values (4, 4);
-insert into closure(parent_id, child_id) values (5, 5);
--- B（第二階層）
-insert into closure(parent_id, child_id) values (1, 2);
-insert into closure(parent_id, child_id) values (1, 3);
--- B（第三階層）
-insert into closure(parent_id, child_id) values (1, 4);
-insert into closure(parent_id, child_id) values (2, 4);
-insert into closure(parent_id, child_id) values (1, 5);
-insert into closure(parent_id, child_id) values (2, 5);
+-- 第一階層
+insert into closure(parent_id, child_id, offset) values (1, 1, 0);
+insert into closure(parent_id, child_id, offset) values (2, 2, 0);
+insert into closure(parent_id, child_id, offset) values (3, 3, 0);
+insert into closure(parent_id, child_id, offset) values (4, 4, 0);
+insert into closure(parent_id, child_id, offset) values (5, 5, 0);
+-- 第二階層
+insert into closure(parent_id, child_id, offset) values (1, 2, 1);
+insert into closure(parent_id, child_id, offset) values (1, 3, 1);
+-- 第三階層
+insert into closure(parent_id, child_id, offset) values (1, 4, 2);
+insert into closure(parent_id, child_id, offset) values (2, 4, 2);
+insert into closure(parent_id, child_id, offset) values (1, 5, 2);
+insert into closure(parent_id, child_id, offset) values (2, 5, 2);
 
 -- メッセージID1に紐づくレコードを取得
-select message.text, closure.parent_id, closure.child_id from message
+select message.text, closure.parent_id, closure.child_id, closure.offset from message
 left join closure on message.id = closure.child_id 
 where closure.parent_id = 1;
 
--- # text, parent_id, child_id
--- '1: 昨日何時に寝た？', '1', '1'
--- '2: >>1 ８時', '1', '2'
--- '3: >>1 １１時', '1', '3'
--- '4: >>2 早い！', '1', '4'
--- '5: >>2 なんでそんな早い？', '1', '5'
+-- # text, parent_id, child_id, offset
+-- '1: 昨日何時に寝た？', '1', '1', '0'
+-- '2: >>1 ８時', '1', '2', '1'
+-- '3: >>1 １１時', '1', '3', '1'
+-- '4: >>2 早い！', '1', '4', '2'
+-- '5: >>2 なんでそんな早い？', '1', '5', '2'
+
