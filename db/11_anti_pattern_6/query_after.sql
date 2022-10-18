e3456ae9c8a08eab2cdd9d5f3c27beec0c93ddc1
Naoki Kaneko <lpeopleocomevtogethere@gmail.com>
2022-10-18 20:44:05 +0900

    微修正

--- db/11_anti_pattern_6/query_after.sql
+++ db/11_anti_pattern_6/query_after.sql
@@ -1,27 +1,27 @@
-drop table if exists status;
 drop table if exists student;
+drop table if exists status;
+
+create table if not exists status(
+    id int AUTO_INCREMENT,
+    name varchar(255),
+    PRIMARY KEY(id)
+);
 
 create table if not exists student (
     id int AUTO_INCREMENT,
-    name varchar,
+    name varchar(255),
     status_id int,
     PRIMARY KEY(id),
-    FOREIGN KEY status_id REFERENCES status(id)
+    FOREIGN KEY (status_id) REFERENCES status(id)
 );
-
-create table if not exists status{
-    id int AUTO_INCREMENT,
-    name varchar,
-    PRIMARY KEY(id)
-}
-
-insert into student (name, status_id) values ("Aさん", 1);
-insert into student (name, status_id) values ("Bさん", 3);
-insert into student (name, status_id) values ("Cさん", 3);
 
 insert into status(name) values ("studying");
 insert into status(name) values ("graduated");
 insert into status(name) values ("teigaku");
+
+insert into student (name, status_id) values ("Aさん", 1);
+insert into student (name, status_id) values ("Bさん", 3);
+insert into student (name, status_id) values ("Cさん", 3);
 
 -- teigakuをsuspendedに変更
 update status set name = "suspended" where id = 3;
