09e0897a31ba1a4dd14e06bbe16e54eef076473d
Naoki Kaneko <lpeopleocomevtogethere@gmail.com>
2023-01-15 16:16:54 +0900

    微修正

--- test/1_test_jest/functions.ts
+++ test/1_test_jest/functions.ts
@@ -1,20 +1,19 @@
-// 配列の平均
+import { AddressApiService } from "./addressApiService";
+
+// 課題4
+
 export const averageOfArray = (numbers: number[]): number => {
   return numbers.reduce((a: number, b: number): number =>
     a + b, 0) / numbers.length;
 };
 
-// 配列の平均（非同期）
-export const AsyncAverageOfArray = async (numbers: number[]): Promise<number> => {
+export const asyncAverageOfArray = async (numbers: number[]): Promise<number> => {
   return new Promise((resolve) => {
     resolve(averageOfArray(numbers));
   })
 };
 
-import { AddressApiService } from "./addressApiService";
-
-// 郵便番号から住所を取得する。もしnullならエラー
-export const getPrefecture = async (zipcode: number): Promise<string> => {
+export const getPrefectureThrowIfNull = async (zipcode: number): Promise<string> => {
   const addressApiService = new AddressApiService();
   const prefecture = await addressApiService.getPrefecture(zipcode);
   if (!prefecture) {
